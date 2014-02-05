36d31accd9e639708193f66ddc8ffbd98e9a0c67
Hackbright Student <info@hackbrightacademy.com>
2014-02-05 12:40:33 -0800

    First version, fixed arithmetic functions

---
+++ arithmetic.py
@@ -0,0 +1,27 @@
+def add(num1, num2):
+    #return 10
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    #pass
+    return num1 * num2
+
+# Ask instructors about decimals
+def divide(num1, num2):
+    x = float(num1) / num2
+    return x
+
+def square(num1):
+    return num1 * num1
+
+def cube(num1):
+    return num1 * num1 * num1 
+
+def power(num1, num2):
+    return pow(num1, num2)
+
+def mod(num1, num2):
+    return num1 % num2
